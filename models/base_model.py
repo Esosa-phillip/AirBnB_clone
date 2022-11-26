90bbb93bd90a58244f337b96182f1222950cc269
Esosa Omoigui <esosa.phillip@yahoo.com>
2022-11-26 23:51:33 +0100

    Function Update

--- models/base_model.py
+++ models/base_model.py
@@ -1,76 +1,58 @@
-#!/usr/bin/python3
-
-"""Model BaseModel Parent of all classes"""
-from datetime import datetime
+#!/usr/bin/pyhon3
+"""
+Parent class that will inherit
+"""
 import uuid
+from datetime import datetime
+from models import storage
 
 
 class BaseModel:
-    """Base class for AirBnB clone project
-    Methods:
-        __init__(self, *args, **kwargs)
-        __str__(self)
-        __save(self)
-        __repr__(self)
-        to_dict(self)
+    """Defines all common attributes/methods
     """
-
     def __init__(self, *args, **kwargs):
-        """Initializes the attributes:
-        random uuid, date create or update"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if "created_at" == key:
-                    self.created_at = datetime.strptime(
-                            kwargs["created_at"],
-                            "%Y-%m-%dT%H:%M:%S.%f")
-                elif "update_at" == key:
-                    self.updated_at = datetime.strptime(
-                            kwargs["updated_at"],
-                            "%Y-%m-%dT%H:%M:%S.%f")
-                elif "__class__" == key:
-                    # we are not doing anything with class
-                    pass
-                else:
-                    setattr(self, key, value)
-        else:
-            # Generate random UUID
+        """initializes all attributes
+        """
+        if not kwargs:
             self.id = str(uuid.uuid4())
-            # assign current date time when an instance is created
-            self.created_at = datetime.now
-            self.updated_at = datetime.now
-            # if it is a new inheritance add to the models
-            from models.__init__ import storage
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
             storage.new(self)
+        else:
+            f = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(kwargs[key], f)
+                if key != '__class__':
+                    setattr(self, key, value)
 
     def __str__(self):
-        """Info about models in string format"""
-        return ('[{}] ({}) {}'.format(self.__class__.__name__,
-                self.id, self.__dict__))
-
-    def __repr__(self):
-        """Returns string representation"""
-        return (self.__str__())
+        """returns class name, id and attribute dictionary
+        """
+        class_name = "[" + self.__class__.__name__ + "]"
+        dct = {k: v for (k, v) in self.__dict__.items() if (not v) is False}
+        return class_name + " (" + self.id + ") " + str(dct)
 
     def save(self):
-        """Updates the public instance attriute updated_at
-        with the current datetime"""
-        self.update_at = datetime.now()
-        from models.__init__ import storage
+        """updates last update time
+        """
+        self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values of
-        __dict__ of the instance"""
+        """creates a new dictionary, adding a key and returning
+        datemtimes converted to strings
+        """
+        new_dict = {}
 
-        # Define the dictionary that adds classs name as  element
-        dic = {}
-        dic["__class__"] = type(self).__name__
-        # Loop over the dict items
-        # Convert create_at and update_at to ISO format
-        for key, val in self.__dict__.items():
-            if isinstance(val, (datetime, )):
-                dic[key] = val.isoformat()
+        for key, values in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                new_dict[key] = values.strftime("%Y-%m-%dT%H:%M:%S.%f")
             else:
-                dic[key] = val
-        return dic
+                if not values:
+                    pass
+                else:
+                    new_dict[key] = values
+        new_dict['__class__'] = self.__class__.__name__
+
+        return new_dict
