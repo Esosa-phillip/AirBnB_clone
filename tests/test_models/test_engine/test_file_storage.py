354e603c37468a3ef43066953b41347def45efec
Esosa Omoigui <esosa.phillip@yahoo.com>
2022-11-27 20:52:26 +0100

    Test File Storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 """ Module of Unittests """
 import unittest
+from os import path
+from models import storage
+from models.user import User
+from models.city import City
+from models.state import State
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models import storage
